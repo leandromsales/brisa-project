e5bfecdfa65a71fb09b7e940c6361a1b4b96780e
Daniel San <daniel.sam123@hotmail.com>
2014-11-08 19:07:18 -0300

    Update gen_brisa._pri.py script
    It was necessary to change the script that generates the file "brisa.pri"
    to add the right way when compiled in a unix environment.
    Was also corrected the brisa.pro to correctly call the gen_brisa_pri.py script.

--- brisa-src/Brisa/scripts/gen_brisa_pri.py
+++ brisa-src/Brisa/scripts/gen_brisa_pri.py
@@ -1,13 +1,24 @@
 import os
 
 file_content = """#This file was auto-generated. Don't edit this.
-BASEDIR = {0}  
-device {{
+
+BASEDIR = {0}
+
+INCLUDEPATH += $$BASEDIR/include
+unix {{
+    message("BRisa Unix config.")
     QT += network xml
-    INCLUDEPATH += $$BASEDIR/include
-    LIBS += -lBrisa
-    LIBS +=  -L$$BASEDIR/arm/so.le-v7-g    
-}}\n""".format(str(os.getcwd()))
+    LIBS += -lbrisa
+    LIBS += -L$$BASEDIR/../build-brisa-Desktop_Qt_5_3_GCC_64bit-Debug
+}}
+
+device {{
+    message("BRisa Device config.")
+    QT += network xml
+    LIBS += -lbrisa
+    LIBS += -L$$BASEDIR/arm/so.le-v7-g
+}}
+""".format(str(os.getcwd()))
 
 #print (file_content)
 f = open(os.path.join(os.getcwd(), "brisa.pri"), "w")
