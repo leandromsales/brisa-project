36119fe11c84c98d542ea8bb7cfc1da8a4f94c02
Larissa Artemis <larissartemis@gmail.com>
2015-05-26 06:52:21 -0300

    upnp/controlpoint/icon commented

--- brisa-src/Brisa/src/upnp/controlpoint/iconsales.h
+++ brisa-src/Brisa/src/upnp/controlpoint/iconsales.h
@@ -28,14 +28,38 @@ public:
     ~IconSales();
     IconSales & operator=(const IconSales &);
 
+    /*!
+     * Set value of \a baseURL.
+     */
     void setBaseUrl(const QUrl &);
+    /*!
+     * Get value of \a baseURL.
+     */
     QUrl getBaseUrl();
+    /*!
+     * Try download icon and return true, if sucessful and false, otherwise.
+     */
     bool downloadIt();
+    /*!
+     * Get URL of icon.
+     */
     QUrl getUrl();
+    /*!
+     * Return a QTemporaryFile contains \a content.
+     */
     QTemporaryFile *getContent();
 
+    /*!
+     * Add an attribute and your value in QHash \a attributes.
+     */
     bool setAttribute(const QString &, const QString &);
+    /*!
+     * Return value of \param attributeName.
+     */
     QString getAttribute(const QString &);
+    /*!
+     * Return a QHash contains all attributes.
+     */
     QHash<QString, QString> getAttributes() const;
 
     typedef enum {
