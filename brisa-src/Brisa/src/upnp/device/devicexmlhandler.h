725869c2a1782c56de9f27b1b6bc2bf1c6ab36b3
Larissa Artemis <larissartemis@gmail.com>
2015-05-28 16:08:22 -0300

    upnp/device/devicexmlhandler commented

--- brisa-src/Brisa/src/upnp/device/devicexmlhandler.h
+++ brisa-src/Brisa/src/upnp/device/devicexmlhandler.h
@@ -14,9 +14,15 @@ class Device;
 
 class DeviceXMLHandler {
 public:
+    /*!
+     * Open a \param file and write description of \param device as XML.
+     */
     void xmlGenerator(Device *device, QFile *file);
 
 private:
+    /*!
+     * A private method used by \a xmlGenerator to get descripton of \param device.
+     */
     void writeDevice(Device *device);
 
     QXmlStreamWriter *writer;
