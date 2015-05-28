21b2855ebf4bbf40b12fbcbf52eba3acf00edda6
Larissa Artemis <larissartemis@gmail.com>
2015-05-28 16:44:23 -0300

    upnp/device/eventsubscription commented.

--- brisa-src/Brisa/src/upnp/device/eventsubscription.h
+++ brisa-src/Brisa/src/upnp/device/eventsubscription.h
@@ -17,12 +17,25 @@ class EventSubscription: public AbstractEventSubscription {
 Q_OBJECT
 
 public:
+    /*!
+     * Constructor. Timeout smaller than zero means infinite.
+     */
 	EventSubscription(const QString &sid, const QStringList &callbackUrls,
-			const int &timeout = -1, // <0 = infinite
+            const int &timeout = -1,
 			QObject *parent = 0);
-
-	void renew(const int &newTimeout = -1); // <0 = infinite
+    /*!
+     * Renew date as current date and timeout as \param newTimeout.
+     * Timeout smaller than zero means infinite.
+     */
+    void renew(const int &newTimeout = -1);
+    /*!
+     * Return a HTTP Response with code 200 (OK). Also set header of message
+     * with date, server, SID, UUID and timeout.
+     */
 	brisa::shared::webserver::http::HttpResponse getAcceptSubscriptionResponse() const;
+    /*!
+     * Return a HTTP Response with code 200 (OK).
+     */
 	brisa::shared::webserver::http::HttpResponse getAcceptUnsubscriptionResponse() const;
 };
 
