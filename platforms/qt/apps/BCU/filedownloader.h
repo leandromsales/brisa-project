82ec94f496ac6a4019fcf0ec5b82b7ba6ff0841e
Larissa Artemis <larissartemis@gmail.com>
2015-08-20 17:13:43 -0300

    Implemented download of images and .compe files. Not tested when bcu and bca are on different computers yet, only when they are on same machine.

---
+++ platforms/qt/apps/BCU/filedownloader.h
@@ -0,0 +1,26 @@
+#include <QObject>
+#include <QByteArray>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+
+class FileDownloader : public QObject
+{
+    Q_OBJECT
+public:
+    FileDownloader();
+    explicit FileDownloader(QUrl imageUrl);
+    virtual ~FileDownloader();
+    QByteArray downloadedData() const;
+    void setURL(QUrl url);
+
+signals:
+    void downloaded();
+
+private slots:
+    void fileDownloaded(QNetworkReply* pReply);
+
+private:
+    QNetworkAccessManager m_WebCtrl;
+    QByteArray m_DownloadedData;
+};
