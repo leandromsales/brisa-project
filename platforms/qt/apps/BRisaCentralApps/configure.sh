0fe7089f7dd7c46f2209841ea2be7c4ecba5fa80
BrunoGeorgevich <brunogeorgevich@hotmail.com>
2016-03-25 17:57:55 -0300

    Add script to download dependencies

---
+++ platforms/qt/apps/BRisaCentralApps/configure.sh
@@ -0,0 +1,2 @@
+cd assets/cpp/ && git clone https://github.com/brunogeorgevich/brunocppessencials essencials &&
+cd ../qml/Components/ && git clone https://github.com/brunogeorgevich/brunocomponents essencials && cd ../../../../
